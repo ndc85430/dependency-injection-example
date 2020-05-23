d617d5d90abf76dfe2617ed226d54a6af0b5a0e7
ndc <65802587+ndc85430@users.noreply.github.com>
2020-05-23 09:19:07 +0100

    Remove separate implementation file for greet function

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
-CXX=clang++
+CXX=g++
 CXXFLAGS=-std=c++11 -Wall -pedantic
 
 greeter:
-	$(CXX) $(CXXFLAGS) greeter.cc greet.cc -o greeter
+	$(CXX) $(CXXFLAGS) greeter.cc -o greeter
 
 test:
-	$(CXX) $(CXXFLAGS) test_greet.cc greet.cc -o test_greet
+	$(CXX) $(CXXFLAGS) test_greet.cc -o test_greet
 	./test_greet
 
 clean:
--- greet.h
+++ greet.h
@@ -1,6 +1,14 @@
 #ifndef GREET_H
 #define GREET_H
 
-void greet(int hour_of_day, std::ostream& output_stream);
+void greet(int hour_of_day, std::ostream& output_stream) {
+  if (hour_of_day < 12) {
+    output_stream << "Good morning";
+  } else if (hour_of_day < 18) {
+    output_stream << "Good afternoon";
+  } else {
+    output_stream << "Good evening";
+  }
+}
 
 #endif
